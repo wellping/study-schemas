a12dc2fa9b960956198e0764fd33f2c4e3846391
Yifei He <yifei@hesyifei.com>
2021-12-20 13:23:04 -0800

    jest `collectCoverage`

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,11 @@ module.exports = {
   verbose: true,
   testMatch: ["**/src/__tests__/**/*.(spec|test).[jt]s?(x)"],
   setupFiles: ["<rootDir>/src/__tests__/setupGlobalVariables.js"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "utility/**/*.{js,jsx,ts,tsx}",
+    "!**/__tests__/**",
+    "!**/node_modules/**",
+  ],
 };

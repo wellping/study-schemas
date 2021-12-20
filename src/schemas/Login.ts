740d4626f18ba697dcdb9916f48bc0565887ee8c
Yifei He <yifei@hesyifei.com>
2021-12-20 12:36:48 -0800

    Delete Login.ts

--- src/schemas/Login.ts
+++
@@ -1,7 +0,0 @@
-import * as z from "zod";
-
-export const LoginSchema = z.object({
-  username: z.string().nonempty(),
-  password: z.string().nonempty(),
-  studyFileURL: z.string().url().nonempty(),
-});
